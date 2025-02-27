52d138604038e2e9facb6b1a6c9466a934692117
Joshua Blais <josh@joshblais.com>
2025-02-27 07:22:38 -0700

    daily

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -7,7 +7,7 @@
    '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "e57616014f09f5d5351c364a44d8bde5baac6d410cfcff5ab21fb0795abc6e1e" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("/home/joshua/org/projects.org" "/home/joshua/org/calendar.org" "/home/joshua/org/todo.org"))
+   '("~/org/contacts.org" "/home/joshua/org/projects.org" "/home/joshua/org/calendar.org" "/home/joshua/org/todo.org"))
  '(package-selected-packages
    '(org-gtasks emacs-everywhere templ-ts-mode multi-vterm protobuf-mode jabber elpher org-roam-ui dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company lsp-ui company hover))
  '(warning-suppress-types
