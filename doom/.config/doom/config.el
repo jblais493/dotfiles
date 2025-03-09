85e016683fff8ab7caf001cd3132a4a315c463be
Joshua Blais <josh@joshblais.com>
2025-03-08 17:14:01 -0700

    update fonts, Alegreya for writing

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -10,10 +10,9 @@
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
-(setq doom-font (font-spec :family "GeistMono Nerd Font" :size 15))
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+(setq doom-font (font-spec :family "GeistMono Nerd Font" :size 15)
+      doom-variable-pitch-font (font-spec :family "Alegreya" :size 18)
+      doom-big-font (font-spec :family "GeistMono Nerd Font" :size 22))
 ;;   presentations or streaming.
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
