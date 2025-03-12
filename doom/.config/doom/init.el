f03e64f379ab917e2aa90aa8475b1ad63107f7c9
Joshua Blais <josh@joshblais.com>
2025-03-12 10:32:44 -0600

    end and addin posting function

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -137,7 +137,6 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
@@ -160,12 +159,13 @@
         +journal
         +roam2
         +hugo
+        +noter
         +dragndrop
+        +pandoc
         +present)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -180,7 +180,6 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
        (yaml +tree-sitter)             ; JSON, but readable
 
        :email
