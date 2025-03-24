dc0fa50e83ecebefabb17a31a7812d49596a241e
Joshua Blais <josh@joshblais.com>
2025-03-24 09:44:47 -0600

    done refile

--- doom/.config/doom/lisp/done-refile.el
+++ doom/.config/doom/lisp/done-refile.el
@@ -1,60 +1,38 @@
-;;; ../../dotfiles/doom/.config/doom/lisp/done-refile.el -*- lexical-binding: t; -*-
+;;; done-refile.el --- Refile items to done.org under today's date -*- lexical-binding: t; -*-
 
 (defun my/move-to-done-org ()
   "Move the current org heading to done.org under today's date."
   (interactive)
   (let* ((done-file (expand-file-name "~/org/done.org"))
-         (today-heading (format-time-string "* %Y-%m-%d %A"))
-         ;; Get the heading components
-         (heading-components (org-heading-components))
-         (level (nth 0 heading-components))
-         (todo-state (nth 2 heading-components))
-         (priority (nth 3 heading-components))
-         (heading-text (nth 4 heading-components))
-         ;; Construct the full heading text including DONE state if it's a task
-         (full-heading (concat
-                        (if todo-state
-                            (if (string= todo-state "DONE")
-                                "DONE "
-                              "DONE ")
-                          "")
-                        (if priority (format "[#%s] " priority) "")
-                        heading-text))
-         ;; Get the entire subtree content
-         (subtree-content (buffer-substring-no-properties
-                           (line-beginning-position)
-                           (save-excursion (org-end-of-subtree t) (point))))
-         ;; Extract just the content without the heading line
-         (content-body (progn
-                         (string-match "\n\\(.*\\)" subtree-content)
-                         (or (match-string 1 subtree-content) "")))
-         ;; Create the entry to add to done.org
-         (entry (format "** %s\n   CLOSED: %s\n%s"
-                        full-heading
-                        (format-time-string "[%Y-%m-%d %a %H:%M]")
-                        content-body)))
+         (today-heading (format-time-string "* %Y-%m-%d %A")))
 
-    ;; Write to done.org
-    (with-temp-buffer
-      (when (file-exists-p done-file)
-        (insert-file-contents done-file))
+    ;; First, mark the task as DONE if it's not already
+    (when (org-entry-is-todo-p)
+      (org-todo 'done))
+
+    ;; Add CLOSED property if it doesn't exist
+    (unless (org-entry-get nil "CLOSED")
+      (org-add-planning-info 'closed (org-current-effective-time)))
+
+    ;; Ensure done.org exists and has today's date heading
+    (with-current-buffer (find-file-noselect done-file)
       (goto-char (point-min))
-
       ;; Find or create today's heading
-      (if (search-forward today-heading nil t)
-          (forward-line 1)
+      (unless (re-search-forward (concat "^" (regexp-quote today-heading) "$") nil t)
         (goto-char (point-max))
         (unless (bolp) (insert "\n"))
-        (insert today-heading "\n"))
+        (insert today-heading "\n")
+        (save-buffer)))
 
-      ;; Insert our entry
-      (insert entry)
-
-      ;; Write back to file
-      (write-region (point-min) (point-max) done-file nil 'quiet))
-
-    ;; Delete the original subtree
-    (org-cut-subtree)
+    ;; Use org-refile to move the subtree
+    (let* ((rfloc (with-current-buffer (find-file-noselect done-file)
+                    (goto-char (point-min))
+                    (re-search-forward (concat "^" (regexp-quote today-heading) "$"))
+                    (list today-heading
+                          done-file
+                          nil
+                          (point)))))
+      (org-refile nil nil rfloc))
 
     (message "Task moved to done.org under %s" today-heading)))
 
