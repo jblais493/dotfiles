07f95e3f055b103b767602f43a21d189be09178c
Joshua Blais <josh@joshblais.com>
2025-04-18 18:49:22 -0600

    popup scratch more like emacs-everywhere

--- doom/.config/doom/lisp/popup-scratch.el
+++ doom/.config/doom/lisp/popup-scratch.el
@@ -1,36 +1,58 @@
 ;;; ../../Dotfiles/doom/.config/doom/lisp/popup-scratch.el -*- lexical-binding: t; -*-
 
 (defun popup-scratch-for-web ()
-  "Create a popup frame with a scratch buffer for web text editing.
+  "Create a popup frame with an org buffer for web text editing.
 Designed specifically for GNOME Wayland with Doom Emacs spell checking."
   (interactive)
-  (let ((buffer (get-buffer-create "*web-compose*")))
+  (let* ((buffer-name "*web-compose*")
+         (buffer (get-buffer-create buffer-name)))
     (with-current-buffer buffer
       (erase-buffer)
-      ;; Use text mode
-      (text-mode)
-
+      ;; Use org mode
+      (org-mode)
       ;; For Doom Emacs, enable appropriate spell checking mode
       (when (fboundp 'spell-checking-enable)
         (spell-checking-enable))
-
       ;; Add header with instructions
-      (insert "# Compose your text below, then press C-c C-c when done.\n")
-      (insert "# Use z= on misspelled words to see correction suggestions.\n")
-      (insert "# Text will be copied to clipboard for pasting.\n\n")
+      (insert "#+TITLE: Web Composition\n")
+      (insert "#+AUTHOR: Emacs User\n\n")
+      (insert "* Instructions\n")
+      (insert "- Press C-c C-c when done to copy text and close\n")
+      (insert "- Use z= on misspelled words to see correction suggestions\n")
+      (insert "- Code blocks will be executed with C-c C-c while cursor is in them\n")
+      (insert "- Text will be copied to clipboard for pasting\n\n")
+      (insert "* Your Text\n\n")
 
       ;; Define our finish function
       (fset 'web-compose-finish
             (lambda ()
               (interactive)
-              ;; Extract content (skipping header comments)
-              (let ((content (buffer-substring-no-properties
-                              (save-excursion
-                                (goto-char (point-min))
-                                (forward-line 4)
-                                (point))
-                              (point-max))))
+              ;; Execute all source blocks first
+              (save-excursion
+                (goto-char (point-min))
+                (when (and (fboundp 'org-babel-execute-buffer)
+                           (y-or-n-p "Execute all code blocks before copying?"))
+                  (org-babel-execute-buffer)))
 
+              ;; Extract content (just the "Your Text" section)
+              (let ((content (save-excursion
+                               (let ((start nil) (end nil))
+                                 ;; Find the Your Text heading
+                                 (goto-char (point-min))
+                                 (re-search-forward "^\\* Your Text$" nil t)
+                                 (forward-line 1)
+                                 ;; Skip any blank lines
+                                 (while (and (not (eobp))
+                                             (looking-at "^$"))
+                                   (forward-line 1))
+                                 (setq start (point))
+                                 ;; Take everything until the end of buffer
+                                 (goto-char (point-max))
+                                 (setq end (point))
+                                 ;; Extract the content
+                                 (buffer-substring-no-properties start end)))))
+                ;; Trim whitespace
+                (setq content (string-trim content))
                 ;; Copy to Emacs clipboard
                 (kill-new content)
                 ;; For Wayland - use wl-copy which is the most compatible
@@ -38,20 +60,33 @@ Designed specifically for GNOME Wayland with Doom Emacs spell checking."
                   (call-process "wl-copy" nil nil nil content))
                 ;; Message user about next steps
                 (message "Text copied to clipboard! Ready to paste with Ctrl+V")
-
                 ;; Store frame to close
                 (let ((frame-to-close (selected-frame)))
-
                   ;; Close frame after a short delay
                   (run-with-timer 0.5 nil
                                   (lambda ()
                                     (delete-frame frame-to-close)))))))
+
       ;; Bind our function to the local map
-      (local-set-key (kbd "C-c C-c") 'web-compose-finish)
+      (local-set-key (kbd "C-c C-c")
+                     (lambda ()
+                       (interactive)
+                       ;; If in a src block, execute it, otherwise finish
+                       (if (and (fboundp 'org-in-src-block-p)
+                                (org-in-src-block-p))
+                           (org-babel-execute-src-block)
+                         (web-compose-finish))))
+
+      (local-set-key (kbd "C-c C-k") (lambda ()
+                                       (interactive)
+                                       (let ((frame-to-close (selected-frame)))
+                                         (run-with-timer 0.1 nil
+                                                         (lambda ()
+                                                           (delete-frame frame-to-close))))))
 
       ;; Set up mode line to indicate Doom/Evil spell check is available
       (setq mode-line-format
-            (list "-- WEB COMPOSE (WAYLAND) -- Use z= for spelling -- C-c C-c when done ")))
+            (list "-- WEB COMPOSE (ORG MODE) -- Use z= for spelling -- C-c C-c to execute/finish ")))
 
     ;; Create the frame
     (let ((frame (make-frame `((name . "Web Compose")
@@ -63,23 +98,104 @@ Designed specifically for GNOME Wayland with Doom Emacs spell checking."
                                (tool-bar-lines . 0)))))
       ;; Set up the frame
       (select-frame frame)
-      (switch-to-buffer "*web-compose*")
-      ;; Position cursor after comments
+      (switch-to-buffer buffer-name)
+
+      ;; Try to insert text from clipboard/selection
+      (save-excursion
+        (goto-char (point-min))
+        (search-forward "* Your Text")
+        (forward-line 2)
+        (let ((start-pos (point))
+              (selection-text nil))
+
+          ;; Try to get text from primary selection
+          (when-let ((selection (gui-get-selection 'PRIMARY 'UTF8_STRING)))
+            (when (not (string-empty-p selection))
+              (setq selection-text selection)))
+
+          ;; If nothing from primary selection, try clipboard
+          (when (and (not selection-text) (eq start-pos (point)))
+            (when-let ((clipboard (gui-get-selection 'CLIPBOARD 'UTF8_STRING)))
+              (when (not (string-empty-p clipboard))
+                (setq selection-text clipboard))))
+
+          ;; If still nothing, try wl-paste for Wayland
+          (when (and (not selection-text) (eq start-pos (point)) (executable-find "wl-paste"))
+            (with-temp-buffer
+              (when (= 0 (call-process "wl-paste" nil t nil))
+                (setq selection-text (buffer-string)))))
+
+          ;; Insert the text, preserving src blocks
+          (when selection-text
+            (insert selection-text))))
+
+      ;; Position cursor after instructions
       (goto-char (point-min))
-      (forward-line 4)
-      ;; Use external window manager tools to center the frame
-      ;; This replaces the previous centering method
+      (search-forward "* Your Text")
+      (forward-line 2)
+
+      ;; Try different centering methods for Wayland
       (run-with-timer
        0.3 nil
        (lambda ()
          (when (frame-live-p frame)
-           ;; Try different centering methods based on available tools
            (cond
-            ;; First try wmctrl which works well with GNOME
+            ;; For sway
+            ((executable-find "swaymsg")
+             (shell-command "swaymsg '[title=\"Web Compose\"]' move position center"))
+            ;; For GNOME/KDE Wayland using wmctrl (if it works with Wayland)
             ((executable-find "wmctrl")
              (shell-command "wmctrl -r 'Web Compose' -e 0,-1,-1,-1,-1"))
+            ;; For Wayland using wtype
+            ((executable-find "wtype")
+             ;; This is less reliable but might work in some cases
+             (shell-command "wtype -M alt -P space -m alt -p space"))))))
 
-            ;; Fall back to xdotool if available
-            ((executable-find "xdotool")
-             (shell-command
-              "xdotool search --name 'Web Compose' windowmove $(xdotool getdisplaygeometry | awk '{print $1/2-400, $2/2-200}')")))))))))
+      ;; Enable org babel languages that might be used
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '((shell . t)
+         (python . t)
+         (emacs-lisp . t)))
+
+      ;; Set sensible org-babel defaults
+      (setq-local org-confirm-babel-evaluate nil) ; Don't confirm execution
+
+      ;; Use transient map for special first-key behavior
+      (let ((keymap (make-sparse-keymap)))
+        (define-key keymap (kbd "DEL") (lambda ()
+                                         (interactive)
+                                         (delete-region (point-min) (point-max))
+                                         (goto-char (point-min))
+                                         (insert "#+TITLE: Web Composition\n")
+                                         (insert "#+AUTHOR: Emacs User\n\n")
+                                         (insert "* Instructions\n")
+                                         (insert "- Press C-c C-c when done to copy text and close\n")
+                                         (insert "- Use z= on misspelled words to see correction suggestions\n")
+                                         (insert "- Code blocks will be executed with C-c C-c while cursor is in them\n")
+                                         (insert "- Text will be copied to clipboard for pasting\n\n")
+                                         (insert "* Your Text\n\n")
+                                         (search-forward "* Your Text")
+                                         (forward-line 2)))
+        (define-key keymap (kbd "C-SPC") (lambda ()
+                                           (interactive)
+                                           (delete-region (point-min) (point-max))
+                                           (goto-char (point-min))
+                                           (insert "#+TITLE: Web Composition\n")
+                                           (insert "#+AUTHOR: Emacs User\n\n")
+                                           (insert "* Instructions\n")
+                                           (insert "- Press C-c C-c when done to copy text and close\n")
+                                           (insert "- Use z= on misspelled words to see correction suggestions\n")
+                                           (insert "- Code blocks will be executed with C-c C-c while cursor is in them\n")
+                                           (insert "- Text will be copied to clipboard for pasting\n\n")
+                                           (insert "* Your Text\n\n")
+                                           (search-forward "* Your Text")
+                                           (forward-line 2)))
+        (set-transient-map keymap))
+
+      ;; Enable org-tempo for easy block insertion if available
+      (when (fboundp 'org-tempo-setup)
+        (org-tempo-setup))
+
+      ;; Message about usage
+      (message "Type your text and press C-c C-c when done (C-c C-k to cancel)"))))
